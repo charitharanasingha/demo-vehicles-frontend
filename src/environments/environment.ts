d00124318de562e3196356ecc521c166f6520a52
Charitha Ranasingha <charitharanasingha@gmail.com>
2023-05-14 04:06:07 +0530

    revert : non ssl link added

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://hq5ndvuil2.execute-api.eu-central-1.amazonaws.com/dev'
+  apiUrl: 'https://hq5ndvuil2.execute-api.eu-central-1.amazonaws.com/dev'
 };
